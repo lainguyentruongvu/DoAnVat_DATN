549757df9254e7671155199e404977288cf08d64
lainguyentruongvu <truongvuback1t@gmail.com>
2023-11-13 22:06:05 +0700

    update login
    13/11

--- src/main/java/poly/store/controller/Admincontroller.java
+++ src/main/java/poly/store/controller/Admincontroller.java
@@ -24,4 +24,10 @@ public class Admincontroller {
 	public String order() {
 		return "admin/order/index";
 	}
+	@RequestMapping("/admin/weight")
+	public String weight() {
+		return "admin/weight/index";
+	}
+	
+	
 }
